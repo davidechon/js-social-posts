4f53dac01e63273695361b34e8a7e6fae63ee89e
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-04-18 16:18:27 +0200

    recap generale

--- main.js
+++ main.js
@@ -21,6 +21,7 @@
 // > 1. Formattare le date in formato italiano (gg/mm/aaaa)
 // > 2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
 // > 3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like
 
 ////////////////////////////////////////////////////////
 // Milestone 1
@@ -124,26 +125,97 @@ posts.forEach((element)=>{
       console.log(litTemplate);
 });
 
-// Milestone 3 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// > 3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like
 
-// AddLike
+////////////////////////////////////////////////////////
+// Cosa devo fare:
+// [_] 1. Richiamare gli elementi della collection
+// [_] 2. Convertire la collection in un array
+// [_] 3. Richiamare gli elementi interessati al "Mi Piace"
+// [_] 4. Creare evento al click
+// [_] 5. Aggiungere 1 unità al contatore
+// [_] 6. Creare un array che contenga gli id dei posts dove ho aggiunto il "Mi Piace"
+// [_] 7. Bonus 1 - Formattare le date in formato italiano (gg/mm/aaaa)
+// [_] 8. Bonus 2 - Gestire l'immagine profilo mancante con le iniziali dell'utente
+// [_] 9. Bonus 3 - Creare un evento contrario al "Mi Piace", togliendo il colore e togliendo 1 unità
+////////////////////////////////////////////////////////
+
+
+
+// Richiamo gli elementi della Collection
 const itemsCollection = document.querySelectorAll('a.like-button.js-like-button');
-// console.log("querySelectorAll itemsCollection 00-A",itemsCollection)
-// console.log("querySelectorAll itemsCollection 01-A",itemsCollection[0])
-// console.log("querySelectorAll itemsCollection 01-A",itemsCollection[1])
-// console.log("querySelectorAll itemsCollection 01-A",itemsCollection[2])
-// console.log("querySelectorAll itemsCollection 01-A",itemsCollection[3])
-// console.log("querySelectorAll itemsCollection 01-A",itemsCollection[4])
+console.log("stampa collection 00",itemsCollection)
+console.log("querySelectorAll itemsCollection 01-A",itemsCollection[0])
+console.log("querySelectorAll itemsCollection 01-A",itemsCollection[1])
+console.log("querySelectorAll itemsCollection 01-A",itemsCollection[2])
+console.log("querySelectorAll itemsCollection 01-A",itemsCollection[3])
+console.log("querySelectorAll itemsCollection 01-A",itemsCollection[4])
+
+
+// const collectionToArray = Array.prototype.slice.call( itemsCollection );
+// console.log("collection to array 01 ",collectionToArray);
+
+// collectionToArray = document.querySelectorAll('a.like-button.js-like-button');
+// console.log("collection to array 02 ",collectionToArray);
+// console.log("collection to array 01 ",collectionToArray[1]);
+
+// collectionToArray.addEventListener('click', addLike());
+
+const addId = [];
 
 function addLike(){
-    itemsCollection.forEach((item)=>{
-        item.addEventListener('click', function(){
-            item.classList.add("like-button--liked");
-        })
-    })
-};
-addLike();
+    // alert('questo funziona')
+    // collectionToArray.classList.add("like-button--liked");
+    // collectionToArray.setAttribute('class', 'like-button--liked');
+    // collectionToArray.innerHTML = `
+    // <a class="like-button like-button--liked js-like-button" href="#" data-postid="${element.id}">
+    //                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    //                         <span class="like-button__label">Mi Piace</span>
+    //                     </a>
+    // `
+    // console.log("stampa addLike 04", collectionToArray)
+    // addId = posts.likes++;
+}
+
+
+
+
+// const nuArray=[];
+// for(let i = 0; i < collectionToArray.length; i++){
+//     i.push = nuArray;
+//     console.log(nuArray)
+// }
+// array.forEach(function(currentValue, index, arr), thisValue)
+
+
+
+// for(let i = 0; i < collectionToArray.length; i++){
+//     collectionToArray[i].classList.add('like-button--liked');
+//     console.log("questa è la collection 02 ",collectionToArray[i])
+// }
+// console.log("questo è fuori dal for 03 ", collectionToArray)
+
+// function addLike(){
+//     itemsCollection.forEach((item)=>{
+//         item.addEventListener('click', function(){
+//             item.classList.add("like-button--liked");
+//         })
+//     })
+// };
+//  addLike();
+
 
 
 // Counter ++
 // document.addEventListener('click');
+// Creare un array che contenga gli id dei likes aggiunti
+
+
+/* <div class="likes__counter">
+    Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+</div> */
+
+// let addPlus = posts[likes] 
+// addPlus++
+// console.log(addPlus)
